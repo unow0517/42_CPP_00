632f3fc67ada8d9a8c1a86f6abe3cf0bb3c9ee4a
unow0517 <unow1992@gmail.com>
2024-08-01 21:38:33 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cctype>
+
+char to_upper(char c)
+{
+	return std::toupper(static_cast<unsigned char>(c));
+}
+int main(int argc, char **argv)
+{
+	int i,j = 0;
+
+	while (++i < argc)
+	{
+		while (argv[i][j])
+		{
+			std::cout << to_upper(argv[i][j]) ;
+			j++;
+		}
+		j = 0;
+		if (i + 1 != argc)
+			std::cout << ' ' ;
+	}
+
+	return 0;
+}
